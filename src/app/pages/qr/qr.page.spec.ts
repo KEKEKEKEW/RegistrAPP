8b185669d737139adf1fa64b66f61396b7e1aa10
Sewennn <ma.acum@duocuc.cl>
2023-10-18 20:23:57 -0300

    tercer commit

--- src/app/pages/qr/qr.page.spec.ts
+++ src/app/pages/qr/qr.page.spec.ts
@@ -5,11 +5,15 @@ describe('QRPage', () => {
   let component: QRPage;
   let fixture: ComponentFixture<QRPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [QRPage],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(QRPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/pages/qr/qr.page.ts
+++ src/app/pages/qr/qr.page.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { BrowserMultiFormatReader, Result, BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'app-qr',
@@ -8,28 +7,88 @@ import { Router } from '@angular/router';
   styleUrls: ['./qr.page.scss'],
 })
 export class QRPage implements OnInit {
-  code: any;
+  qrResult: string = ''; // Variable para almacenar el resultado del escaneo de QR
+  codeReader: BrowserMultiFormatReader;
+  isScanning: boolean = false;
+  userData: {
+    nombre: string;
+    apellido: string;
+    carrera: string;
+    rut: string;
+    datosEscaneados?: string[]; // Agrega datosEscaneados con el operador '?' para que sea opcional
+  } = {
+    nombre: '',
+    apellido: '',
+    carrera: '',
+    rut: '',
+  };// Inicializa 'userData' con propiedades predeterminadas
 
-  constructor(private barcodeScanner: BarcodeScanner,private router: Router) { }
+  @ViewChild('videoElement', { static: true }) videoElement: ElementRef | undefined;
 
-  ngOnInit() {
-    this.barcodeScanner.scan().then(barcodeData => {
-      this.code = barcodeData.text;
-      console.log('Barcode data', this.code);
-      let mensaje = this.code.split(',')
-
-      var clases = {
-        profesor: mensaje[0],
-        hora: mensaje[1],
-        sala: mensaje[2],
-        dia: mensaje[3]
-      }
-
-      localStorage.setItem('clases',JSON.stringify(clases));
-
-     }).catch(err => {
-         console.log('Error', err);
-     });
+  constructor() {
+    this.codeReader = new BrowserMultiFormatReader();
   }
 
+  ngOnInit() {
+    this.getUserInfo(); // Cargamos los datos del alumno al inicio
+  }
+
+  openScanner() {
+    if (this.videoElement) {
+      // Define las opciones del escáner, como el formato de código QR que deseas escanear
+      const hints = new Map<BarcodeFormat, any>();
+      hints.set(BarcodeFormat.QR_CODE, {});
+
+      // Inicia el escáner
+      this.codeReader
+        .decodeFromInputVideoDevice(undefined, this.videoElement.nativeElement)
+        .then((result: Result) => {
+          // Cuando se escanea un código QR, esto se ejecuta
+          this.qrResult = result.getText(); // Almacena el resultado del escaneo
+
+          // Divide los datos escaneados por comas
+          const datosEscaneados = this.qrResult.split(',');
+
+          // Combina los datos escaneados con los datos del alumno en una sola estructura
+          this.userData = {
+            ...this.userData,
+            datosEscaneados: datosEscaneados,
+          };
+
+          // Guarda todo en el Local Storage
+          localStorage.setItem('userData', JSON.stringify(this.userData));
+
+          this.isScanning = false; // Detiene el escáner
+          this.getUserInfo(); // Llama a getUserInfo() después de obtener los datos escaneados
+        })
+        .catch((error: any) => {
+          console.error(error);
+          this.isScanning = false; // Detiene el escáner en caso de error
+        });
+
+      this.isScanning = true; // Inicia el escáner y muestra la visualización de video
+    }
+  }
+
+  closeScanner() {
+    // Detén el escáner
+    this.codeReader.reset();
+    this.qrResult = ''; // Restablece el resultado
+  }
+
+  getUserInfo() {
+    // Obtén la información del usuario desde el Local Storage
+    const userInfoString = localStorage.getItem('usuario');
+    
+    if (userInfoString) { // Verifica si userInfoString no es nulo
+      const userInfo = JSON.parse(userInfoString);
+  
+      // Combina los datos del alumno y los datos escaneados en 'userData'
+      this.userData = {
+        ...this.userData, // Mantén los datos anteriores en 'userData'
+        ...userInfo, // Agrega los datos del alumno
+        datosEscaneados: this.qrResult.split(','), // Agrega los datos escaneados
+      };
+    }
+  }
 }
